d3e370d4c6f8a2c0931a6bd064d5456453a3184c
Gustavo Rojas Valdivia <doc.gustavo.rojas.va@unifranz.edu.bo>
2025-01-26 16:57:06 -0400

    Modified SecurityConfig.java to allow access only to the Swagger route

--- src/main/java/com/dh/notesapp/security/SecurityConfig.java
+++ src/main/java/com/dh/notesapp/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,12 +20,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
+                        ).permitAll() // Permitir acceso a Swagger sin autenticación
+                        .requestMatchers("/api/auth/**").permitAll() // Permitir autenticación sin restricciones
+                        .anyRequest().authenticated() // Proteger cualquier otro endpoint
                 )
-                .httpBasic();
+                .formLogin(login -> login
+                        .loginPage("/login")  // Ruta personalizada de login si es necesario
+                        .defaultSuccessUrl("/swagger-ui/index.html", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout.permitAll());
+
         return http.build();
     }
 
